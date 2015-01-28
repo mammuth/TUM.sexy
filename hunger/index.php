0fe52e5d8b855568995e54dc4354e2cbeb0bd747
Max Muth <mail@maxi-muth.de>
2015-01-28 12:04:08 +0100

    Improved hunger. It now takes the current weeks plan, even when it's set as the upcoming weeks plan.

--- hunger/index.php
+++ hunger/index.php
@@ -30,10 +30,14 @@ function redirect($url, $statusCode = 303)
 $links = crawl_page("http://www.betriebsrestaurant-gmbh.de/index.php?id=91");
 
 foreach ($links as $file) {
+    // Get the FMI plans
     if (strpos(strtolower($file), '.pdf') !== FALSE && strpos($file, '_FMI_') !== FALSE) {
-        // current link is MI pdf
-        redirect("http://www.betriebsrestaurant-gmbh.de/".$file);
-
+        // Get the current weeks plan by commparing the week numbers. Because they're often late in changing their links...
+        $weekNumber = date("W"); 
+        if ($weekNumber === substr($file,16,2)){
+            redirect("http://www.betriebsrestaurant-gmbh.de/".$file);
+        }
+        echo ("Seems like the \'Betriebsrestaurant-GmbH changed something...<br/>Take a look at <a href=\"http://www.betriebsrestaurant-gmbh.de/index.php?id=91)\">http://www.betriebsrestaurant-gmbh.de/index.php?id=91</a>");
     }
 }
 
