9bcaf99bbe31f2c3cd2c1c134e88980748d738b2
kordianbruck <kb@wdkk.de>
2015-11-09 11:17:48 -0600

    Switched hunger project over to use some awesome twig templating

---
+++ setup.php
@@ -0,0 +1,23 @@
+<?php
+
+//Send some important headers and setup php
+ini_set('error_reporting', 0);
+ini_set('display_errors', 0);
+date_default_timezone_set('Europe/Berlin');
+
+header('Content-Type: text/html; charset=UTF-8');
+mb_internal_encoding('UTF-8');
+
+session_start();
+
+//Include libs
+include __DIR__ . '/vendor/autoload.php';
+
+
+//Startup Twig
+Twig_Autoloader::register();
+$loader = new Twig_Loader_Filesystem(__DIR__ . '/tpl');
+$twig = new Twig_Environment($loader, [
+    'cache' => __DIR__ . '/tmp/compile',
+    'debug' => true
+]);
