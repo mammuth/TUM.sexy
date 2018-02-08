714766f5d97b92a4f29f1002af6ebe10f8cdfd8d
Max Muth <kontakt@maxi-muth.de>
2018-02-08 12:58:26 +0100

    Add additional test case

--- tests/General/MainTest.php
+++ tests/General/MainTest.php
@@ -4,7 +4,10 @@ class MainTest extends \PHPUnit\Framework\TestCase {
 
     public function testRouteResolving() {
         $router = new Route();
+        // Normal redirect
         $this->assertEquals('http://tum.sexy/hunger', $router->getTargetOfSub('hunger', null));
+        // Subsubdomain redirect to moodle
+        $this->assertContains('moodle.tum.de', $router->getTargetOfSub('anal', 'm'));
     }
 
     public function testJsonOutput() {
