298b4a05dbd1046e1ac57278b3dcce07d4b64418
Jonathan <web@jonathanmengedoht.com>
2018-02-12 11:36:53 +0100

    use default parameter

--- tests/General/MainTest.php
+++ tests/General/MainTest.php
@@ -5,7 +5,7 @@ class MainTest extends \PHPUnit\Framework\TestCase {
     public function testRouteResolving() {
         $router = new Route();
         // Normal redirect
-        $this->assertEquals('http://tum.sexy/hunger', $router->getTargetOfSub('hunger', null));
+        $this->assertEquals('http://tum.sexy/hunger', $router->getTargetOfSub('hunger'));
         // Subsubdomain redirect to moodle
         $this->assertContains('https://www.moodle.tum.de/course/view.php?id=36704', $router->getTargetOfSub('anal', 'm'));
     }
