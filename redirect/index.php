59a88d98d3f055a409d8fc622ae7c93a722cd19b
Kordian Bruck <k@bruck.me>
2018-02-18 01:01:16 +0100

    test rewrite rules

--- redirect/index.php
+++ redirect/index.php
@@ -1,5 +1,8 @@
 <?php
 
+var_dump($_SERVER);
+die();
+
 include __DIR__ . '/routes.php';
 $routeHandler = new Route();
 header('Location: ' . $routeHandler->getTargetOfSub(mb_strtolower($_GET['redirectUrl']), mb_strtolower($_GET['siteType']))); // Function does check if GET parameter is set / empty
