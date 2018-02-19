e2ed0244b4b163cd857ee10272a0fa081349ddf8
zsewa <zsewa@outlook.de>
2018-02-19 14:08:54 +0100

    requested changes for error checking
    - added condition when cache or webpage returns an empty string
    - added comment: being implicit what line 15 - 20 does
    - added comment: being implicit why I check for an empty $links array
    - added comment: being implicit why I check if array position is empty using the user provided GET_variable

--- shuttle/index.php
+++ shuttle/index.php
@@ -3,13 +3,28 @@ use ThauEx\SimpleHtmlDom\SHD;
 
 SHD::$fileCacheDir = "cache";
 $html = SHD::fileGetHtml('http://wzw.tum.de/index.php?id=416');
+if (empty($html)) {
+	// something wrong with fileGetHTML
+	// redirect to overview page than
+	header('Location: http://wzw.tum.de/index.php?id=416');
+	die();
+}
 
 $links = array();
 
 foreach($html->find('div[id=c2489]') as $element) {
+	// inside the div#c2489 there should be one to three (or more) links
 	foreach($element->find('a') as $element2) {
        		$links[] = $element2->href;
 	}
+}
+
+if (empty($links)) {
+	// not able to extract link-elements from html
+	// may happen when faculty changes content management system
+	// redirect to overview page than
+	header('Location: http://wzw.tum.de/index.php?id=416');
+	die();
 }
 
 // The load variable allows the user to load the schedule for the upcoming week using '?load=1'
@@ -19,15 +34,9 @@ if(empty($_GET['load'])) {
     $load = $_GET['load'];    
 }
 
-if (empty($links)) {
-	// not getting any links? Faculty reconstructed the page? 
-	// redirect to overview page than
-	header('Location: http://wzw.tum.de/index.php?id=416');
-	die();
-}
-
 if ($links[$load] == "") {
-	// provided an load variable that is not in the array
+	// user provided a load variable that is not in the array
+	// may happen when there is no plan uploaded for upcoming week but user is using ?load=1
 	// load default than
 	$load = 0;
 }
