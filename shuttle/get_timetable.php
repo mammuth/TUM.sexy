15ad8b4042e7f36a7667d9bff7ae7b8983578574
zsewa <zsewa@outlook.de>
2018-02-19 11:42:17 +0100

    Create get_timetable.php
    This PHP script redirects to the pdf timetable of the WZW<<>>GAR shuttle bus.

---
+++ shuttle/get_timetable.php
@@ -0,0 +1,20 @@
+<?php
+include 'simple_html_dom.php';
+
+$html = file_get_html('http://wzw.tum.de/index.php?id=416');
+
+$links = array();
+
+foreach($html->find('div[id=c2489]') as $element) 
+	foreach($element->find('a') as $element2) 
+       		$links[] = $element2->href;
+
+// The load variable allows the user to load the schedule for the upcoming week using '?load=1'
+$load = $_GET['load'];
+
+if ($links[$load] == "") {
+	$load = 0;
+}
+
+header('Location: http://www.wzw.tum.de/'.$links[$load]);
+die();
