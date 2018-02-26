d1dd5beaab2de4f2c4a51b11ddf03d9a61999d62
Kordian Bruck <k@bruck.me>
2018-02-26 19:48:57 +0100

    #101 refactor shuttle shortcut

--- shuttle/index.php
+++ shuttle/index.php
@@ -1,45 +1,35 @@
 <?php
+
 use ThauEx\SimpleHtmlDom\SHD;
 
+define('SHUTTLE_URL', 'http://wzw.tum.de/index.php?id=416');
+
 SHD::$fileCacheDir = "cache";
-$html = SHD::fileGetHtml('http://wzw.tum.de/index.php?id=416');
+$html = SHD::fileGetHtml(SHUTTLE_URL);
 if (empty($html)) {
-	// something wrong with fileGetHTML
-	// redirect to overview page than
-	header('Location: http://wzw.tum.de/index.php?id=416');
-	die();
+    // something wrong with fileGetHTML - redirect to overview page than
+    header('Location: ' . SHUTTLE_URL);
+    die();
 }
 
-$links = array();
-
-foreach($html->find('div[id=c2489]') as $element) {
-	// inside the div#c2489 there should be one to three (or more) links
-	foreach($element->find('a') as $element2) {
-       		$links[] = $element2->href;
-	}
+$links = [];
+foreach ($html->find('div[id=c2489]') as $element) {
+    // inside the div#c2489 there should be one to three (or more) links
+    foreach ($element->find('a') as $e) {
+        $links[] = $e->href;
+    }
 }
 
 if (empty($links)) {
-	// not able to extract link-elements from html
-	// may happen when faculty changes content management system
-	// redirect to overview page than
-	header('Location: http://wzw.tum.de/index.php?id=416');
-	die();
+    // not able to extract link-elements from html - may happen when faculty changes content management system
+    header('Location: ' . SHUTTLE_URL);
+    die();
 }
 
 // The load variable allows the user to load the schedule for the upcoming week using '?load=1'
-if(empty($_GET['load'])) {
-    $load = 0;    
-} else {
-    $load = $_GET['load'];    
-}
-
-if ($links[$load] == "") {
-	// user provided a load variable that is not in the array
-	// may happen when there is no plan uploaded for upcoming week but user is using ?load=1
-	// load default than
-	$load = 0;
+$load = 0;
+if (!empty($_GET['load']) && array_key_exists($_GET['load'], $load)) {
+    $load = $_GET['load'];
 }
 
 header('Location: http://www.wzw.tum.de/' . $links[$load]);
-die();
