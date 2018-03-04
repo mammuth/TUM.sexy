00e6e7d06664aef42653026ad7fe658220aaa97d
Jan Philip Bernius <janphilip@bernius.net>
2018-03-04 19:53:58 +0100

    Remove Cats Query Parameter

--- index.php
+++ index.php
@@ -7,12 +7,9 @@ include __DIR__ . '/redirect/routes.php';
 $Router = new Route();
 $vars = ['routes' => $Router->getResolvedArrays()];
 
-//We always want the parameter in the URL
-if (!isset($_GET['cats'])) {
-    header('Location: /index.php?cats=0');
-    die();
-} elseif (isset($_GET['cats']) && $_GET['cats'] !== '0') {
-    $vars['cats'] = true; //Enables the script in the template
+$vars['cats'] = "hidden";
+if (isset($_GET['cats']) && $_GET['cats'] !== '0') {
+    $vars['cats'] = "show"; //Enables the script in the template
 }
 
 //Donation Key
---
+++ js/cats.js
@@ -0,0 +1,3 @@
+window.onload = function () {
+    document.body.background = 'http://thecatapi.com/api/images/get?format=src&type=gif';
+};
