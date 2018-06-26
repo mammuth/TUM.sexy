66cf83292eb0f2913cda38fd84df3c38db9d3f15
vollkorntomate <38211057+vollkorntomate@users.noreply.github.com>
2018-06-26 15:49:53 +0200

    updated erapraktikum link (#149)

--- redirect/routes.php
+++ redirect/routes.php
@@ -198,7 +198,7 @@ class Route {
         ],
         'erapraktikum' => [
             'description' => 'Praktikum - Einführung in die Rechnerarchitektur',
-            'target'      => 'https://www.lrr.in.tum.de/lehre/sommersemester-18/praktika/praktikum-rechnerarchitektur/',
+            'target'      => 'https://www.caps.in.tum.de/lehre/ss18/praktika/praktikum-rechnerarchitektur/',
         ],
         'ged'          => [
             'description' => 'Game Engine Design',
