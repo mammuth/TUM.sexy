8a11f0f9f6f2858c99f826bbcc3aa95e7940e12f
Jigao Luo <luojigao@outlook.com>
2018-11-12 19:25:32 +0100

    fix the informatics master's curriculum link

--- redirect/routes.php
+++ redirect/routes.php
@@ -154,7 +154,7 @@ class Route {
         ],
         'ma-sp'        => [
             'description' => 'Studienplan M.Sc. Informatik',
-            'target'      => 'https://www.in.tum.de/fuer-studierende/master-studiengaenge/informatik/studienplan/fpo-2007-und-fpsos-seit-2012.html',
+            'target'      => 'https://www.in.tum.de/fuer-studierende/master-studiengaenge/informatik/studienplan/fpso-2018/',
         ],
         'ge-sp'        => [
             'description' => 'Studienplan B.Sc. Informatik: Games Engineering',
