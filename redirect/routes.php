2b596d4943327a5b728826dbc6a1ce294a35178c
Max Eisner <4730112+maxOS11@users.noreply.github.com>
2019-04-23 16:07:28 +0200

    Updated redirects + Added gadjudge (#213)
    * Update routes.php
    
    * Update routes.php
    
    * Added gadjudge to redirect to TUMJudge GAD course
    
    * Updated redirects for GAD and ERA-Praktikum

--- redirect/routes.php
+++ redirect/routes.php
@@ -185,8 +185,12 @@ class Route {
         ],
         'gad'          => [
             'description' => 'Grundlegende Algorithmen und Datenstrukturen',
-            'target'      => 'http://campar.in.tum.de/Chair/TeachingSs18GAD',
+            'target'      => 'https://www.in.tum.de/cg/teaching/summer-term-19/grundlagen-algorithmen-und-datenstrukturen-in0007/',
             'moodle_id'   => '40888',
+        ],
+        'gadjudge'          => [
+            'description' => 'TUMJudge - Grundlegende Algorithmen und Datenstrukturen',
+            'target'      => 'https://judge.in.tum.de/gad/public/',
         ],
         'linalg'       => [
             'description' => 'Lineare Algebra für Informatik',
@@ -206,7 +210,7 @@ class Route {
         ],
         'erapraktikum' => [
             'description' => 'Praktikum - Einführung in die Rechnerarchitektur',
-            'target'      => 'https://www.caps.in.tum.de/lehre/ws18/praktika/praktikum-rechnerarchitektur/',
+            'target'      => 'https://www.caps.in.tum.de/lehre/ss19/praktika/era/',
         ],
         'ged'          => [
             'description' => 'Game Engine Design',
@@ -427,7 +431,7 @@ class Route {
             'info1', 'pgdp', 'era', 'ds', 'carlos',
         ],
         '2. Semester' => [
-            'eist', 'gad', 'erapraktikum', 'linalg', 'ged',
+            'eist', 'gad', 'gadjudge', 'erapraktikum', 'linalg', 'ged',
         ],
         '3. Semester' => [
             'anal', 'info2', 'db', 'gbs',
