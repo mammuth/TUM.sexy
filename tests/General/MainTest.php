49aff1a6bea541169594ccb69cecb7d635ba8983
Joscha Henningsen <joscha.henningsen@tum.de>
2019-11-03 12:45:44 +0100

    Update non moodle test case

--- tests/General/MainTest.php
+++ tests/General/MainTest.php
@@ -15,8 +15,7 @@ class MainTest extends \PHPUnit\Framework\TestCase {
         $this->assertStringContainsString('https://www.moodle.tum.de/course/view.php?id=49592', $router->getTargetOfSub('mgbs.tum.sexy'));
 
         // Normal redirect still works, even if it has moodle type assigned
-	// gbs.tum.sexy points to moodle as well.
-        $this->assertStringContainsString('https://www.moodle.tum.de/course/view.php?id=49592', $router->getTargetOfSub('gbs.tum.sexy'));
+        $this->assertStringContainsString('https://db.in.tum.de/teaching/ws1920/grundlagen/?lang=de', $router->getTargetOfSub('db.tum.sexy'));
     }
 
     public function testJsonOutput() {
