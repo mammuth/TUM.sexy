d45b283044a0ee3aacda0152059e70bcb9688403
Valentin Hartig <valentin.hartig@tum.de>
2020-04-17 17:28:36 +0200

    Updated links for GAD, GRNVS and EIST to 2020

--- redirect/routes.php
+++ redirect/routes.php
@@ -194,12 +194,12 @@ class Route {
         ],
         'eist'         => [
             'description' => 'Einführung in die Softwaretechnik',
-            'target'      => 'https://ase.in.tum.de/eist',
+            'target'      => 'https://ase.in.tum.de/lehrstuhl_1/teaching/summer-2020/1111-introduction-to-software-engineering-eist-summer-2020',
         ],
         'gad'          => [
             'description' => 'Grundlegende Algorithmen und Datenstrukturen',
-            'target'      => 'https://www.in.tum.de/cg/teaching/summer-term-19/grundlagen-algorithmen-und-datenstrukturen-in0007/',
-            'moodle_id'   => '40888',
+            'target'      => 'https://ciip.in.tum.de/teaching/gad_ss20.html',
+            'moodle_id'   => '53358',
         ],
         'gadjudge'     => [
             'description' => 'TUMJudge - Grundlegende Algorithmen und Datenstrukturen',
@@ -215,7 +215,7 @@ class Route {
         ],
         'grnvs'        => [
             'description' => 'Grundlagen Rechnernetze und Verteilte Systeme',
-            'target'      => 'https://www.net.in.tum.de/teaching/ss19/grnvs.html',
+            'target'      => 'https://www.net.in.tum.de/teaching/ss20/grnvs.html',
         ],
         'pgm'          => [
             'description' => "Probabilistic Graphical Models in Computer Vision",
