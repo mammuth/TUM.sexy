307de24650c477d945b001bc90dc0976f303fca0
Joscha Henningsen <44805696+joschahenningsen@users.noreply.github.com>
2021-02-07 15:19:47 +0100

    use local storage for pins (#44)

--- js/pins.js
+++ js/pins.js
@@ -1,20 +1,7 @@
 let pins = [];
 
-function setCookie(name, value, exDays) {
-    let d = new Date();
-    d.setTime(d.getTime() + (exDays * 24 * 60 * 60 * 1000));
-    let expires = "expires=" + d.toUTCString();
-    document.cookie = name + "=" + value + ";" + expires + ";path=/;SameSite=Strict";
-}
-
-function getCookie(name) {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(';').shift();
-}
-
 function removePin(elem) {
-    let remove = elem.parentElement.innerText.split("\n")[0]
+    let remove = elem.parentElement.children[1].innerHTML
     let newPins = []
     pins.forEach(pin => {
         if (pin.n !== remove) {
@@ -22,7 +9,7 @@ function removePin(elem) {
         }
     })
     pins = newPins
-    setCookie("pins", JSON.stringify(pins), 100)
+    localStorage.setItem("pins", JSON.stringify(pins))
     render()
 }
 
@@ -31,7 +18,7 @@ function addPin(elem) {
     let pinName = parentContent.substr(0, parentContent.lastIndexOf(" — "))
     let pinUrl = parentContent.substr(parentContent.lastIndexOf(" — ") + 3, parentContent.length - (13 + parentContent.lastIndexOf(" — ")))
     pins.push({"n": pinName, "l": pinUrl})
-    setCookie("pins", JSON.stringify(pins), 100)
+    localStorage.setItem("pins", JSON.stringify(pins))
     elem.parentElement.removeChild(elem)
     render()
 }
@@ -47,8 +34,7 @@ function render() {
         </div>`).join('')}` + '<div class="placeholder"></div>'.repeat(3)//seems off, but appears to be common praxis
 }
 
-
 window.onload = function () {
-    pins = JSON.parse(getCookie("pins"))
+    pins = JSON.parse(localStorage.getItem("pins") === null ? "[]" : localStorage.getItem("pins"))
     render()
 }
